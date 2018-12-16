6afdfd62625aedc9ab22e4feb3234ddabcfb518f
Stephen Wiggins <steve.wiggins7@gmail.com>
2018-12-15 23:37:43 -0500

    fix layouts with css-grid

--- main.js
+++ main.js
@@ -2,3 +2,7 @@ var canvas = document.getElementById("myCanvas");
 var ctx = canvas.getContext("2d");
 ctx.fillStyle = "#9900BB";
 ctx.fillRect(0,0,canvas.width, canvas.height);
+
+function addPC(){
+    console.log("button Pressed");
+}
